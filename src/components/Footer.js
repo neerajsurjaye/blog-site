74a5e5c6be5add485d0612f9a6ba553becfcdb24
neerajsurjaye <neerajsurjaye@gmail.com>
2021-07-29 13:42:33 +0530

    redirects

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 let Footer = () => {
     return <div className="footer">
         <div> &#169;
-            <a href='https://github.com/neerajsurjaye'>Neeraj Surjaye (2021)</a>
+            <a href='https://github.com/neerajsurjaye' target="_blank" rel="noopener noreferrer">Neeraj Surjaye (2021)</a>
         </div>
     </div>
 }
